52af0ee2984f5cd3bf5f92c5eb8ccc705856fa08
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-04-25 16:53:14 +0300

    easy loadable params for pokemons

--- src/app/fetchAndSavePokemonData.js
+++ src/app/fetchAndSavePokemonData.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 
 async function fetchPokemonData() {
   try {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1');
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');
     const data = await response.json();
     const pokemons = data.results;
 
@@ -13,29 +13,29 @@ async function fetchPokemonData() {
       const pokemonData = await pokemonResponse.json();
 
       if (pokemonData) {
-        console.log(pokemonData.abilities); // Выводим имя покемона в консоль
+        console.log(pokemonData); // Выводим имя покемона в консоль
 
          await prisma.pokemon.create({
           data: {
             name: pokemonData.name,
-            abilities: pokemonData.abilities.map(ability => ability.ability.name),
-            types: pokemonData.types.map(type => type.type.name),
-            sprite: pokemonData.sprites.front_default,
-            shinySprite: pokemonData.sprites.front_shiny,
-            locations: pokemonData.location_area_encounters,
-            learnset: pokemonData.moves.map(move => move.move.name),
-            stats: pokemonData.stats,
+           // abilities: pokemonData.abilities.map(ability => ability.ability.name),
+            //types: pokemonData.types.map(type => type.type.name),
+            //sprite: pokemonData.sprites.front_default,
+            //shinySprite: pokemonData.sprites.front_shiny,
+            // locations: pokemonData.location_area_encounters,
+            //learnset: pokemonData.moves.map(move => move.move.name),
+            //stats: pokemonData.stats,
             weight: pokemonData.weight,
             height: pokemonData.height,
             baseExperience: pokemonData.base_experience,
-            forms: pokemonData.forms.map(form => form.name),
-            heldItems: pokemonData.held_items.map(item => item.item.name),
-            generation: pokemonData.species.url,
-            color: pokemonData.color.name,
+           // forms: pokemonData.forms.map(form => form.name),
+           // heldItems: pokemonData.held_items.map(item => item.item.name),
+            //generation: pokemonData.species.url,
+            //color: pokemonData.color.name,
             species: pokemonData.species.name,
             experience: pokemonData.base_experience,
-            moves: pokemonData.moves.map(move => move.move.name),
-            characteristics: pokemonData.abilities.map(ability => ability.ability.name),
+           // moves: pokemonData.moves.map(move => move.move.name),
+           // characteristics: pokemonData.abilities.map(ability => ability.ability.name),
           },
          });
       }
