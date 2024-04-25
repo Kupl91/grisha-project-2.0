cdf09380d555c8b7a9036c9705a5a7f951abc2d3
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-04-25 15:11:38 +0300

    +migrations

---
+++ prisma/migrations/20240425093754_init/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE "Pokemon" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "abilities" TEXT[],
+    "types" TEXT[],
+    "sprite" TEXT NOT NULL,
+    "shinySprite" TEXT NOT NULL,
+    "locations" TEXT[],
+    "learnset" TEXT[],
+    "stats" JSONB NOT NULL,
+    "weight" INTEGER NOT NULL,
+    "height" INTEGER NOT NULL,
+    "baseExperience" INTEGER NOT NULL,
+    "forms" TEXT[],
+    "heldItems" TEXT[],
+    "generation" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "species" TEXT NOT NULL,
+    "experience" INTEGER NOT NULL,
+    "moves" TEXT[],
+    "characteristics" JSONB NOT NULL,
+
+    CONSTRAINT "Pokemon_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
