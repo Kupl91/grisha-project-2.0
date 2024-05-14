eebf8726bc955ebd0625ebeb29063e6acf054dc0
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-05-15 00:00:34 +0300

    + deleteMany for pokemonId, id

--- pages/api/pokemon/[id].ts
+++ pages/api/pokemon/[id].ts
@@ -1,36 +1,37 @@
-// C:\Users\pavel.kuplensky\js\grisha-project\pages\api\pokemon\[id].ts
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query;
 
   if (req.method === 'DELETE') {
     try {
-      await prisma.pokemon.delete({
-        where: {
-          id: Number(id),
-        },
-      });
-      res.status(200).json({ message: 'Покемон успешно удален' });
-    } catch (error) {
-      res.status(500).json({ error: 'Ошибка при удалении покемона' });
+      // Используем транзакцию для удаления записей
+      await prisma.$transaction([
+        // Удаляем все записи в таблице PokemonAbility, у которых pokemonId совпадает с id
+        prisma.pokemonAbility.deleteMany({
+          where: { pokemonId: Number(id) },
+        }),
+        // Удаляем запись в таблице Pokemon по id
+        prisma.pokemon.delete({
+          where: { id: Number(id) },
+        }),
+      ]);
+      
+      res.status(200).json({ message:'Покемон успешно удален'});
+    } catch(error){
+     console.log(error);
+     res.status(500).json({ error:'Ошибка приудалении покемона '});
     }
-  } else if (req.method === 'GET') {
-    const pokemon = await prisma.pokemon.findUnique({
-      where: { id: Number(id) },
-      include: { abilities: { select: { ability: { select: { name: true } } } } }
-    });
+   } else if (req.method === 'GET'){
+    const pokemon=awaitprisma.pokemon.findUnique ({
+       where:{id:Number(id)},
+       include:{abilities:{ select:{ ability :{ select :{ name:true}}}}}
+     });
 
-    if (pokemon) {
-      res.status(200).json(pokemon);
-    } else {
-      res.status(404).json({ message: 'Покемон не найден' });
-    }
-  }
-}
+if(pokemon){
+   res .status(200).json(pokémon)}
+else{
+res .status(404 ).json ({message :'Покемоне найден'})}}}
