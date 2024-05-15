787af99b757562a712826e90d08b1cd0999648aa
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-05-15 18:10:59 +0300

    + post

--- pages/api/pokemon/[id].ts
+++ pages/api/pokemon/[id].ts
@@ -1,3 +1,4 @@
+// C:\Users\pavel.kuplensky\js\grisha-project\pages\api\pokemon\[id].ts
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
 
@@ -6,7 +7,18 @@ const prisma = new PrismaClient();
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query;
 
-  if (req.method === 'DELETE') {
+  if (req.method === 'GET'){
+    const pokemon = await prisma.pokemon.findUnique ({
+       where:{id:Number(id)},
+       include:{abilities:{ select:{ ability :{ select :{ name:true}}}}}
+     });
+
+    if(pokemon){
+       res.status(200).json(pokemon);
+    } else {
+       res.status(404).json({message :'Покемон не найден'});
+    }
+  } else if (req.method === 'DELETE') {
     try {
       // Используем транзакцию для удаления записей
       await prisma.$transaction([
@@ -23,15 +35,28 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       res.status(200).json({ message:'Покемон успешно удален'});
     } catch(error){
      console.log(error);
-     res.status(500).json({ error:'Ошибка приудалении покемона '});
+     res.status(500).json({ error:'Ошибка при удалении покемона '});
     }
-   } else if (req.method === 'GET'){
-    const pokemon=awaitprisma.pokemon.findUnique ({
-       where:{id:Number(id)},
-       include:{abilities:{ select:{ ability :{ select :{ name:true}}}}}
-     });
-
-if(pokemon){
-   res .status(200).json(pokémon)}
-else{
-res .status(404 ).json ({message :'Покемоне найден'})}}}
+  } else if (req.method === 'POST') { // обработка POST-запроса
+    const { id, name, weight, height, species, experience } = req.body;
+  
+    try{
+      const createdPokemon = await prisma.pokemon.create({
+        data: {
+          id,
+          name,
+          weight,
+          height,
+          species,
+          experience
+        }
+      });
+  
+      res.status(201).json(createdPokemon);
+     
+     } catch(error){
+           console.error(error);
+           res.status(500).json({ error:`Ошибка при создании покемона ${error.message}`});
+     }
+  }
+}
