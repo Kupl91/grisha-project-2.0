ae06e45ee8cbfb8da63ee043db30fa2521f545a7
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-05-16 12:01:19 +0300

    +post

--- pages/api/pokemon/create.ts
+++ pages/api/pokemon/create.ts
@@ -1,32 +1,40 @@
-// C:\Users\pavel.kuplensky\js\grisha-project\pages\api\pokemon\create.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const { id, name, weight, height, species, experience } = req.body;
-  
-    try{
-      const createdPokemon = await prisma.pokemon.create({
-        data: {
-          id,
-          name,
-          weight,
-          height,
-          species,
-          experience
-        }
-      });
-  
-      res.status(201).json(createdPokemon);
-     
-     } catch(error){
-           console.error(error);
-           res.status(500).json({ error:`Ошибка при создании покемона ${error.message}`});
+ if (req.method === 'POST') {
+   try {
+     const { name, weight, height, baseExperience, species ,experience ,abilities} = req.body;
+
+     if (!name || !weight || !height || !baseExperience || !species) {
+       return res.status(400).json({ error: "Все поля обязательны" });
      }
-  } else {
-    res.status(405).json({ error: 'Метод не поддерживается' });
+
+     const createdPokemon = await prisma.pokemon.create({
+       data:{
+         name,
+         weight,
+         height,
+         baseExperience,
+         species,
+         experience: experience ?? 0,
+         abilities: abilities && abilities.length ? 
+           { create : abilities.map(a => ({ abilityName:a })) }
+           : undefined
+       }
+     });
+
+    res.status(201).json(createdPokemon);
+
+  } catch (error) {
+    console.error("Ошибка при создании покемона:", error);
+    return res.status(500).json({ error:`Ошибка при создании покемона ${error.message}`});
   }
+ } else {
+   res.setHeader('Allow', ['POST']);
+   return res.status(405).end(`Метод ${req.method} не поддерживается`);
+ }
+
 }
