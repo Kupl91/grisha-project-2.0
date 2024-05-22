1c7857acc57f1469fcc4dfb84e7b27c798ccae32
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-05-22 11:31:54 +0300

    refac pokemonpage

--- pages/pokemons.tsx
+++ pages/pokemons.tsx
@@ -1,464 +1,50 @@
-<<<<<<< Updated upstream
-// grisha-project-2.0\pages\pokemonsv1.tsx
-import React, { useEffect, useState } from 'react';
-
-interface Pokemon {
-  id: number;
-  name: string;
-  weight: number;
-  height: number;
-  baseExperience: number;
-  species: string;
-  experience: number;
-  abilities: { ability: { name: string } }[];
-}
-=======
-//C:\Users\Pavel\Documents\GitHub\grisha-project-2.0\pages\pokemons.tsx
-import React, { useEffect, useState } from 'react';
-import  PokemonList  from '../components/PokemonList';
-import  PokemonForm  from '../components/PokemonForm';
-import  FilterAndSort  from '../components/FilterAndSort';
-import PokemonDetails  from '../components/PokemonDetails';
->>>>>>> Stashed changes
+// C:\Users\pavel.kuplensky\js\grisha-project\pages\pokemons.tsx
+import React, { useEffect } from 'react';
+import FilterAndSort from '../components/FilterAndSort';
+import Pagination from '../components/Pagination';
+import PokemonList from '../components/PokemonList';
+import PokemonForm from '../components/PokemonForm';
+import { usePokemonActions } from './api/PokemonActions';
 
 const PokemonsPage = () => {
-  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-  const [selectedDetail, setSelectedDetail] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-  const [sortType, setSortType] = useState('id');
-  const [filterType, setFilterType] = useState('name');
-  const [filterValue, setFilterValue] = useState('');
-  const [showForm, setShowForm] = useState(false);
-  const [newPokemon, setNewPokemon] = useState({
-    name: '',
-    weight: 0,
-    height: 0,
-    species: '',
-    experience: 0,
-  });
-  const [updateFormOpen, setUpdateFormOpen] = useState(false);
-  const [updatingPokemon, setUpdatingPokemon] = useState(null);
-  const itemsPerPage = 5;
+  const {
+    pokemons,
+    selectedDetail,
+    newPokemon,
+    updatingPokemon,
+    fetchPokemons,
+    handleDetailsClick,
+    handleDeleteClick,
+    handleSubmitClick,
+    handleUpdateSubmit,
+  } = usePokemonActions();
 
   useEffect(() => {
-    const fetchPokemons = async () => {
-      try {
-        const response = await fetch('/api/pokemons');
-<<<<<<< Updated upstream
-        
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-  
-        const data = await response.json();
-        
-        setPokemons(data);
-        setTotalPages(Math.ceil(data.length / itemsPerPage));
-      } catch (error) {
-         console.error("Ошибка при загрузке покемонов:", error.message); 
-      }
-    };
-  
-=======
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        setPokemons(data);
-        setTotalPages(Math.ceil(data.length / itemsPerPage));
-      } catch (error) {
-        console.error("Ошибка при загрузке покемонов:", error.message);
-      }
-    };
-
->>>>>>> Stashed changes
     fetchPokemons();
   }, []);
 
-  const handleDetailsClick = async (id) => {
-    if (selectedDetail && selectedDetail.id === id) {
-      setSelectedDetail(null);
-    } else {
-      setSelectedDetail(null);
-<<<<<<< Updated upstream
-      try{
-     const response=await fetch(`/api/pokemon/${id}`);
-  if(response.ok){
-  const pokemonData=await response.json();
-  setSelectedDetail({
-            id:pokemonData.id,
-  experience:pokemonData.experience,
-  height:pokemonData.height,
-  weight:pokemonData.weight});
-  } else{
-  throw newError ('Не удалось получить информацию о покемоне');
-  }
-  }
-       catch(error){
-  console.error("Ошибка при загрузке данных:",error);   
-  }}
-  };
-
-  const handleDeleteClick = async (id: number) => {
-    try {
-      const response = await fetch(`/api/pokemon/delete?id=${id}`, {
-        method: 'DELETE',
-      });
-      console.log(response);
-      if (response.ok) {
-        setPokemons(pokemons.filter((pokemon) => pokemon.id !== id));
-      } else {
-        throw new Error('Не удалось удалить покемона');
-      }
-    } catch (error) {
-      console.error("Ошибка при удалении покемона:", error);
-    }
-  };
-  
-  const nextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const previousPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
-
-  const handleSortChange = (event) => {
-    setSortType(event.target.value);
-  };
-
-  const handleFilterTypeChange = (event) => {
-    setFilterType(event.target.value);
-  };
-
-  const handleFilterValueChange = (event) => {
-    setFilterValue(event.target.value.toLowerCase());
- };
-
- const sortedAndFilteredPokemons = pokemons
-  .filter((pokemon) => pokemon[filterType]?.toString().toLowerCase().includes(filterValue))
-  .sort((a, b) => {
-    if (sortType === 'name') {
-      return a.name.localeCompare(b.name);
-    } else {
-      return a[sortType] - b[sortType];
-    }
-   });
-
-  const handleCreateClick = () => {
-    setShowForm(true);
-  };
-
-  const handleSubmitClick = async () => {
-    try {
-      console.log(newPokemon);
-      const response = await fetch('/api/pokemon/create', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          name: newPokemon.name,
-          weight: newPokemon.weight,
-          height: newPokemon.height,
-          species: newPokemon.species,
-          experience:newPokemon.experience, 
-        }),
-      });
-    
-      if (response.ok) {
-        const pokemon = await response.json();
-        setPokemons([...pokemons, pokemon]);
-        setShowForm(false);
-      } else {
-        throw new Error('Не удалось создать покемона');
-      }
-    } catch (error) {
-      console.error("Ошибка при создании покемона:", error.message);
-    }
-  };
-  
-=======
-      try {
-        const response = await fetch(`/api/pokemon/${id}`);
-        if (response.ok) {
-          const pokemonData = await response.json();
-          setSelectedDetail({
-            id: pokemonData.id,
-            experience: pokemonData.experience,
-            height: pokemonData.height,
-            weight: pokemonData.weight,
-          });
-        } else {
-          throw new Error('Не удалось получить информацию о покемоне');
-        }
-      } catch (error) {
-        console.error("Ошибка при загрузке данных:", error);
-      }
-    }
-  };
-
-  const handleDeleteClick = async (id: number) => {
-    try {
-      const response = await fetch(`/api/pokemon/delete?id=${id}`, {
-        method: 'DELETE',
-      });
-      if (response.ok) {
-        setPokemons(pokemons.filter((pokemon) => pokemon.id !== id));
-      } else {
-        throw new Error('Не удалось удалить покемона');
-      }
-    } catch (error) {
-      console.error("Ошибка при удалении покемона:", error);
-    }
-  };
-
-  const nextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const previousPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
-
-  const handleSortChange = (event) => {
-    setSortType(event.target.value);
-  };
-
-  const handleFilterTypeChange = (event) => {
-    setFilterType(event.target.value);
-  };
-
-  const handleFilterValueChange = (event) => {
-    setFilterValue(event.target.value.toLowerCase());
-  };
-
-  const sortedAndFilteredPokemons = pokemons
-    .filter((pokemon) => pokemon[filterType].toString().toLowerCase().includes(filterValue))
-    .sort((a, b) => {
-      if (sortType === 'name') {
-        return a.name.localeCompare(b.name);
-      } else {
-        return a[sortType] - b[sortType];
-      }
-    });
-
-  const handleCreateClick = () => {
-    setShowForm(true);
-  };
-
-  const handleSubmitClick = async () => {
-    try {
-      const response = await fetch('/api/pokemon/create', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          name: newPokemon.name,
-          weight: newPokemon.weight,
-          height: newPokemon.height,
-          species: newPokemon.species,
-          experience: newPokemon.experience,
-        }),
-      });
-
-
-  if (response.ok) {
-    const pokemon = await response.json();
-    setPokemons([...pokemons, pokemon]);
-    setShowForm(false);
-  } else {
-    throw new Error('Не удалось создать покемона');
-  }
-} catch (error) {
-  console.error("Ошибка при создании покемона:", error.message);
-}
-  };
-
->>>>>>> Stashed changes
-  const handleUpdateClick = (id: number) => {
-    if (updatingPokemon && updatingPokemon.id === id) {
-      // Если форма обновления уже открыта для этого покемона, закройте ее
-      setUpdatingPokemon(null);
-    } else {
-      // Иначе откройте форму обновления для этого покемона
-      const pokemonToUpdate = pokemons.find((pokemon) => pokemon.id === id);
-<<<<<<< Updated upstream
-      setUpdatingPokemon(pokemonToUpdate ? {...pokemonToUpdate} : null);
-=======
-      setUpdatingPokemon(pokemonToUpdate ? { ...pokemonToUpdate } : null);
->>>>>>> Stashed changes
-    }
-  };
-
-  const handleUpdateSubmit = async () => {
-    try {
-      const response = await fetch('/api/pokemon/update', {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          id: updatingPokemon.id,
-          name: updatingPokemon.name,
-          weight: updatingPokemon.weight,
-          height: updatingPokemon.height,
-          species: updatingPokemon.species,
-          experience: updatingPokemon.experience,
-        }),
-      });
-<<<<<<< Updated upstream
-  
-      if (response.ok) {
-        const updatedPokemon = await response.json();
-        setPokemons(pokemons.map((pokemon) => pokemon.id === updatedPokemon.id ? updatedPokemon : pokemon));
-        setUpdatingPokemon(null);
-      } else {
-        throw new Error('Не удалось обновить покемона');
-      }
-    } catch (error) {
-      console.error("Ошибка при обновлении покемона:", error.message);
-    }
-=======
-
-
-  if (response.ok) {
-    const updatedPokemon = await response.json();
-    setPokemons(pokemons.map((pokemon) => pokemon.id === updatedPokemon.id ? updatedPokemon : pokemon));
-    setUpdatingPokemon(null);
-  } else {
-    throw new Error('Не удалось обновить покемона');
-  }
-} catch (error) {
-  console.error("Ошибка при обновлении покемона:", error.message);
-}
->>>>>>> Stashed changes
-  };
-
-  const handleUpdateInputChange = (event) => {
-    setUpdatingPokemon({
-      ...updatingPokemon,
-      [event.target.name]: event.target.value,
-    });
-  };
-<<<<<<< Updated upstream
-  
-=======
-
->>>>>>> Stashed changes
-  const handleInputChange = (event) => {
-    if (event.target.name === 'id' && Number(event.target.value) < 26) {
-      alert('ID не может быть меньше 26');
-      return;
-    }
-    setNewPokemon({
-      ...newPokemon,
-      [event.target.name]: event.target.value,
-    });
-  };
-
   return (
-    <div style={{ backgroundColor: 'black', color: 'white' }}>
-<<<<<<< Updated upstream
-      <input type="text" onChange={handleFilterValueChange} placeholder="Фильтр..." />
-      <select onChange={handleFilterTypeChange}>
-        <option value="name">Имя</option>
-        <option value="id">ID</option>
-        <option value="height">Высота</option>
-      </select>
-      <select onChange={handleSortChange}>
-        <option value="id">ID</option>
-        <option value="name">Имя</option>
-        <option value="height">Высота</option>
-      </select>
-      {sortedAndFilteredPokemons
-        .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
-        .map((pokemon) => (
-          <div key={pokemon.id} style={{ marginBottom:'10px', display: 'flex', alignItems: 'center' }}>
-            <button onClick={() => handleDeleteClick(pokemon.id)} style={{ marginRight: '10px', padding: '5px 10px', backgroundColor: '#0070f3', color: '#fff', textDecoration: 'none', borderRadius: '5px' }}>Удалить</button>
-            <h2 style={{ marginRight: '10px' }}>{pokemon.name}</h2>
-            <button onClick={() => handleDetailsClick(pokemon.id)} style={{ marginRight: '10px', padding: '5px 10px', backgroundColor: '#0070f3', color: '#fff', textDecoration: 'none', borderRadius: '5px' }}>Детали</button>
-            {selectedDetail && selectedDetail.id === pokemon.id && 
-              (<div>{`ID: ${selectedDetail.id}, Опыт: ${selectedDetail.experience}, Высота: ${selectedDetail.height}, Вес: ${selectedDetail.weight}`}</div>)
-            }
-            <button onClick={() => handleUpdateClick(pokemon.id)} style={{ marginRight: '10px', padding: '5px 10px', backgroundColor: '#0070f3', color: '#fff', textDecoration: 'none', borderRadius: '5px' }}>Обновить</button>
-            {updatingPokemon && updatingPokemon.id === pokemon.id && (
-              <div>
-                <input type="text" name="name" value={updatingPokemon.name} onChange={handleUpdateInputChange} placeholder="Имя" />
-                <input type="number" name="weight" value={updatingPokemon.weight} onChange={handleUpdateInputChange} placeholder="Вес" />
-                <input type="number" name="height" value={updatingPokemon.height} onChange={handleUpdateInputChange} placeholder="Высота" />
-                <input type="text" name="species" value={updatingPokemon.species} onChange={handleUpdateInputChange} placeholder="Вид" />
-                <input type="number" name="experience" value={updatingPokemon.experience} onChange={handleUpdateInputChange} placeholder="Опыт" />
-                <button onClick={handleUpdateSubmit}>Отправить</button>
-
-              </div>
-            )}
-          </div>
-        ))}
-        <button onClick={previousPage} style={{ padding: '5px 10px', backgroundColor: '#0070f3', color: '#fff', textDecoration: 'none', borderRadius: '5px' }}>Предыдущая</button>
-        <button onClick={nextPage} style={{ padding: '5px 10px', backgroundColor: '#0070f3', color: '#fff', textDecoration: 'none', borderRadius: '5px' }}>Следующая</button>
-        <button onClick={handleCreateClick} style={{ padding: '5px 10px', backgroundColor: '#0070f3', color: '#fff', textDecoration: 'none', borderRadius: '5px' }}>Создай</button>
-        {showForm && (
-          <div>
-            <input type="text" name="name" onChange={handleInputChange} placeholder="Имя" />
-            <input type="number" name="weight" onChange={handleInputChange} placeholder="Вес" />
-            <input type="number" name="height" onChange={handleInputChange} placeholder="Высота" />
-            <input type="text" name="species" onChange={handleInputChange} placeholder="Вид" />
-            <input type="number" name="experience" onChange={handleInputChange} placeholder="Опыт" />
-            <button onClick={handleSubmitClick}>Отправить</button>
-          </div>
-        )}
-        <div>Страница {currentPage} из {totalPages}</div>
-      </div>
-    );
-  };
-  
-export default PokemonsPage;
-=======
-      <FilterAndSort
-        filterType={filterType}
-        filterValue={filterValue}
-        handleFilterTypeChange={handleFilterTypeChange}
-        handleFilterValueChange={handleFilterValueChange}
-      />
+    <div>
+      <FilterAndSort />
       <PokemonList
-        pokemons={sortedAndFilteredPokemons}
-        handleDetailsClick={handleDetailsClick}
+        pokemons={pokemons}
         handleDeleteClick={handleDeleteClick}
-      />
-      <PokemonForm
-        showForm={showForm}
-        handleCreateClick={handleCreateClick}
-        handleSubmitClick={handleSubmitClick}
-        handleInputChange={handleInputChange}
-        newPokemon={newPokemon}
-      />
-      <PokemonDetails selectedDetail={selectedDetail} />
-      <PokemonForm
-        showForm={updateFormOpen}
-        handleSubmitClick={handleUpdateSubmit}
-        handleInputChange={handleUpdateInputChange}
+        handleDetailsClick={handleDetailsClick}
+        handleUpdateClick={handleUpdateSubmit}
+        selectedDetail={selectedDetail}
         updatingPokemon={updatingPokemon}
       />
-      <button onClick={previousPage} style={{ padding: '5px 10px', backgroundColor: '#0070f3', color: '#fff', textDecoration: 'none', borderRadius: '5px' }}>Предыдущая</button>
-      <button onClick={nextPage} style={{ padding: '5px 10px', backgroundColor: '#0070f3', color: '#fff', textDecoration: 'none', borderRadius: '5px' }}>Следующая</button>
-      <button onClick={handleCreateClick} style={{ padding: '5px 10px', backgroundColor: '#0070f3', color: '#fff', textDecoration: 'none', borderRadius: '5px' }}>Создай</button>
-      <div>Страница {currentPage} из {totalPages}</div>
+      <Pagination />
+      <PokemonForm
+        handleSubmitClick={handleSubmitClick}
+        handleInputChange={(event) => setNewPokemon({
+          ...newPokemon,
+          [event.target.name]: event.target.value,
+        })}
+        newPokemon={newPokemon}
+      />
     </div>
   );
 };
 
 export default PokemonsPage;
->>>>>>> Stashed changes
